c4b0b95ae0ac9112a06e8548356f1a6fd39804c1
souhailaitsaid <souhailaitsaid@gmail.com>
2025-04-30 20:30:08 +0200

    first commit

--- src/main/java/be/fgov/bosa/chatbot/chatbot/InitData.java
+++ src/main/java/be/fgov/bosa/chatbot/chatbot/InitData.java
@@ -1,28 +1,46 @@
 package be.fgov.bosa.chatbot.chatbot;
 
-import dev.langchain4j.data.document.Metadata;
-import dev.langchain4j.data.embedding.Embedding;
-import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.store.embedding.EmbeddingStore;
-import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import be.fgov.bosa.chatbot.chatbot.models.Chatbot;
+import be.fgov.bosa.chatbot.chatbot.models.Organization;
+import be.fgov.bosa.chatbot.chatbot.repositories.ChatbotRepository;
+import be.fgov.bosa.chatbot.chatbot.repositories.OrganizationRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Component
 @AllArgsConstructor
 public class InitData implements ApplicationRunner {
-    private final EmbeddingModel embeddingModel;
-    private final EmbeddingStoreIngestor embeddingStoreIngestor;
-    private final EmbeddingStore<TextSegment> embeddingStore;
 
-
+    private final ChatbotRepository chatbotRepository;
+    private final OrganizationRepository organizationRepository;
+    static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1); // upper bound is exclusive
+    }
+    public static String generateRandomString(int length) {
+        final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz  ";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        TextSegment segment1 = TextSegment.from("I like football.", Metadata.metadata("userId", "1"));
-        Embedding embedding1 = embeddingModel.embed(segment1).content();
-        embeddingStore.add(embedding1, segment1);
+        Organization organization = organizationRepository.save(Organization.builder().name("Bosa").build());
+        List<Chatbot> bots = chatbotRepository.saveAll(IntStream.range(0, getRandomInt(2, 10))
+                .mapToObj(i -> Chatbot.builder()
+                        .organization(organization)
+                        .description( generateRandomString(getRandomInt(15, 25)))
+                        .name("Bot "+i)
+                        .build())
+                .collect(Collectors.toList()));
     }
 }
